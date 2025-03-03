9d401b187ffe86ddfc06c9cee900d7de5d9061a3
davidjayjackson <46544155+davidjayjackson@users.noreply.github.com>
2025-03-03 18:03:41 -0500

    PG upload using R + RPostgresql
    Long Beach Animal Shelter, FBI Data.

---
+++ load_csv_excel.R
@@ -0,0 +1,40 @@
+# library(tidyverse)
+library(janitor)
+library(RPostgres)
+library(DBI)
+library(readxl)
+rm(list=ls())
+# Establish connection to PostgreSQL database
+con <- dbConnect(
+  RPostgres::Postgres(),
+  dbname = "tidytuesday",
+  host = "localhost",     # e.g., "localhost" or "your-server.com"
+  port = 5432,           # Default PostgreSQL port
+  user = "postgres",
+  password = "dJj135790"
+)
+
+# Check if connection is successful
+if (!dbIsValid(con)) {
+  stop("Failed to connect to the database!")
+} else {
+  print("Connected to PostgreSQL successfully!")
+}
+
+# Load Data
+# Long Beach Animal Shelter Data
+animal_shelter <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-04/longbeach.csv') |> clean_names()
+dbWriteTable(con, "animal_shelter",animal_shelter, overwrite=TRUE,append=FALSE)
+dbListTables(con)
+
+# Agencies from the FBI Crime Data API
+# This week we're exploring data from the FBI Crime Data API! Specifically, 
+# we’re looking at agency-level data across all 50 states in the USA. 
+# This dataset provides details on law enforcement agencies that have submitted data to the 
+# FBI’s Uniform Crime Reporting (UCR) Program and are displayed on the Crime Data Explorer (CDE).
+library(tidytuesdayR)
+tuesdata <- tidytuesdayR::tt_load('2025-02-18')
+FBI <- tuesdata$agencies
+dbWriteTable(con, "FBI",FBI, overwrite=TRUE,append=FALSE)
+
+dbListTables(con)
